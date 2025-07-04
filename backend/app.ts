66cb32b66f342fa15168a060830c76e833b0e366
Poloko-dev <polokonkolanyane92@gmail.com>
2025-07-04 11:49:51 +0200

    fixing cross-site issues

--- backend/app.ts
+++ backend/app.ts
@@ -22,7 +22,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI!,
+    mongoUrl: process.env.MONGO_URI!,
     collectionName: 'sessions',
     ttl: 24 * 60 * 60,
   }),
