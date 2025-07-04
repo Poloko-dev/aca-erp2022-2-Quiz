7621a14315019228915d868eca7f1a276a13e63b
Poloko-dev <polokonkolanyane92@gmail.com>
2025-07-04 11:01:30 +0200

    fixing cross-site issues

--- backend/app.ts
+++ backend/app.ts
@@ -18,9 +18,9 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: false,
+    secure: true,
     maxAge: 1000 * 60 * 60,
-    sameSite: 'lax' 
+    sameSite: 'none' 
   }
 }));
 
